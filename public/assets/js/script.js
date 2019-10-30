8dcac47cc780f266562d28b4d6a3d115fb8ad3b4
eyobdejene <eyob_westerink@hotmail.com>
2019-10-30 22:17:12 +0100

    fix filter visibility items

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,13 +2,13 @@
 
 
 
-new Glide('.glide', {
-    type: 'slider',
-    startAt: 0,
-    perView: 3,
-    rewind: false,
-    keyboard:false
-}).mount();
+// new Glide('.glide', {
+//     type: 'slider',
+//     startAt: 0,
+//     perView: 3,
+//     rewind: false,
+//     keyboard:false
+// }).mount();
 
 
 
@@ -35,41 +35,48 @@ function search(nameKey, array){
 function val() {
     let array = JSON.parse(localStorage.getItem('museum_objects'));
     let filterValue = document.getElementById("filter").value;
-    search(filterValue,array);
 
-    let arrayFilterd = JSON.parse(localStorage.getItem('museum_objects_filterd'));
-    let filter = document.getElementById("filter");
+    if(filterValue != "default") {
+        search(filterValue, array);
 
-    filter.classList.add("active");
+        let arrayFilterd = JSON.parse(localStorage.getItem('museum_objects_filterd'));
+        let filter = document.getElementById("filter");
 
-    if(filter.classList.contains("active")) {
-        // console.log('active');
-        // loop trough all elements and remove show class
-        for (let interger = 0; interger < array.length; interger++) {
-            document.querySelector("[data-id='" + array[interger].id + "']").classList.remove("show");
+        console.log(filterValue);
+
+        filter.classList.add("active");
+
+        if (filter.classList.contains("active")) {
+            // console.log('active');
+            // loop trough all elements and remove show class
+            for (let interger = 0; interger < array.length; interger++) {
+                document.querySelector("[data-id='" + array[interger].id + "']").classList.remove("show");
+            }
+
+            // loop trough all filtered objects and addd show class
+            for (var i = 0; i < arrayFilterd.length; i++) {
+                // console.log(arrayFilterd[i].id);
+                document.querySelector("[data-id='" + arrayFilterd[i].id + "']").classList.add("show");
+                //console.log(document.querySelectorAll("[data-id='"+arrayFilterd[i].id+"']").classList);
+            }
         }
 
-        // loop trough all filtered objects and addd show class
-        for (var i = 0; i < arrayFilterd.length; i++) {
-            // console.log(arrayFilterd[i].id);
-            document.querySelector("[data-id='"+arrayFilterd[i].id+"']").classList.add("show");
-            //console.log(document.querySelectorAll("[data-id='"+arrayFilterd[i].id+"']").classList);
+        // loop through elements in dom and if the element contains a show class remove the class and add hide class
+        let objectsItems = document.getElementsByClassName("objectItem");
+        for (let counter = 0; counter < objectsItems.length; counter++) {
+            console.log(objectsItems[counter].classList);
+            console.log(objectsItems[i].classList.contains('show'));
+            if (!objectsItems[counter].classList.contains('show')) {
+                objectsItems[counter].classList.remove("show");
+                objectsItems[counter].classList.add("hide");
+            }
         }
+
+       // console.log(filterValue)
+        setSelectedIndex(filter, filterValue);
+    }else{
+        resetToVisible();
     }
-
-    // loop through elements in dom and if the element contains a show class remove the class and add hide class
-    let objectsItems = document.getElementsByClassName("objectItem");
-    for (let counter = 0; counter < objectsItems.length; counter++) {
-        console.log(objectsItems[counter].classList);
-        console.log(objectsItems[i].classList.contains('show'));
-        if(!objectsItems[counter].classList.contains('show')){
-            objectsItems[counter].classList.remove("show");
-            objectsItems[counter].classList.add("hide");
-        }
-    }
-
-
-
 
 
 }
@@ -79,6 +86,7 @@ function val() {
 
 function resetToVisible(){
     console.log('reset');
+    let filter = document.getElementById("filter");
     let objectsItems = document.getElementsByClassName("objectItem");
     for (let counter = 0; counter < objectsItems.length; counter++) {
         //console.log(x[i].classList);
@@ -88,6 +96,24 @@ function resetToVisible(){
             objectsItems[counter].classList.remove("hide");
         }
     }
+    filter.classList.remove("active");
+    setSelectedIndex(filter,"default");
+}
+
+
+
+function setSelectedIndex(s, v) {
+
+    for ( var i = 0; i < s.options.length; i++ ) {
+
+        if ( s.options[i].value == v ) {
+            s.options[i].selected = true;
+            return;
+
+        }
+
+    }
+
 }
 
 
